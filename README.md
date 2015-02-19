f00e88e5ce44d8a230c599593b390fd7506835cf
Devin Carr <andromeda@dhcp-10-202-121-160.tamulink.tamu.edu>
2015-02-19 12:58:27 -0600

    Ready for alpha runs.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # echo
-A Twitch.tv IRC echo bot
+A Twitch.tv IRC echo bot.
+
+## Usage
+Echo will listen in on the twitch.tv chat and join in on the spam when it begins. It usually waits for a 1/4 ratio of normal chat to spammed messages to repeat and participate. The bot will also wait up to 10 seconds from when it joins the spam to speak again to make sure not to get too out of control.
+
+## Instructions
+```shell
+$ python kappa.py
+```
+Type in your password using the oauth key generated from [here](http://twitchapps.com/tmi/) and then the channel of the twitch.tv stream.
+
+## License
+MIT - (view LICENSE for more information)
--- kappa.py
+++ kappa.py
@@ -7,15 +7,19 @@ class Kappa:
 	connected = False
 	nickname = 'feedthespam'
 	password = 'oauth:'
-	channels = ['#feedthespam']
+	channel = '#'
+	watcher = None
+	frequency = 4
 
 	def __init__(self):
 		self.begin()
+		self.watcher = Watch(self.frequency)
 		self.socket = socket.socket()
 		self.socket.connect(('irc.twitch.tv', 6667))
 		self.send("PASS %s" % self.password)
 		self.send("NICK %s" % self.nickname)
 
+	def start(self):
 		while True:
 			buf = self.socket.recv(4096)
 			lines = buf.split("\n")
@@ -24,10 +28,11 @@ class Kappa:
 
 				if data == '':
 					continue
-				print "I<", data
+				#print "<", data
 
-				# server ping/pong?
+				# Server Ping/Pong response
 				if data.find('PING') != -1:
+					print '< PING'
 					n = data.split(':')[1]
 					self.send('PONG :' + n)
 				if self.connected == False:
@@ -35,54 +40,99 @@ class Kappa:
 					self.connected = True
 
 				args = data.split(None, 3)
+				# Check for PRIVMSG's not server messages
 				if len(args) != 4:
 					continue
-				ctx = {}
-				ctx['sender'] = args[0][1:]
-				ctx['type']   = args[1]
-				ctx['target'] = args[2]
-				ctx['msg']    = args[3][1:]
 
-				# whom to reply?
-				target = ctx['target']
-				if ctx['target'] == self.nickname:
-					target = ctx['sender'].split("!")[0]
+				if 'PRIVMSG' not in args[1]:
+					continue
 
+				# Add every message that comes into the queue
+				msg = args[3][1:]
+				self.watcher.add(msg)
+
+				# Watch for spamming and then join the fun
+				spam = self.watcher.check()
+				if spam != None:
+					print 'Got some spam: ', spam
+					print 'Joining in..'
+					self.say(spam,self.channel)
 
 	def send(self, msg):
-		print msg
+		print '>', msg
 		self.socket.send(msg+"\r\n")
 
 	def say(self, msg, to):
 		self.send("PRIVMSG %s :%s" % (to, msg))
 
 	def perform(self):
-		for c in self.channels:
-			self.send("JOIN %s" % c)
-			# say hello to every channel
-			self.say('Are we ready? Kappa', c)
+		self.send("JOIN %s" % self.channel)
+		# say hello to every channel
+		self.say('Are we ready? Kappa', self.channel)
 
 	def quit(self):
-		print "Terminating echo.."
-		for c in self.channels:
-			self.say("Echo out.")
-			time.sleep(1)
-			self.send("PART %s" % c)
-
+		print 'Terminating echo..'
+		self.say('Echo out.', self.channel)
+		time.sleep(1)
+		self.send("PART %s" % self.channel)
 
 	def begin(self):
 		print 'Welcome to echo.\n'
-		s = raw_input('Please type in your oauth token here:')
+		print 'We will now be operating at a spam frequency of 1/%s' % self.frequency
+		s = raw_input('\nPlease type in your oauth token here: ')
 		if (s.find('oauth:') != -1):
 			self.password = s
 		else:
 			self.password += s
 
+		self.channel += raw_input('Channel to join: ')
+		print 'Let\'s Begin.'
+
+class Watch:
+	spamQueue = []
+	size = 0
+	repeat = 0
+	timeout = -1
+	timeoutLimit = 10
+	wait = 0
+
+	def __init__(self,maxSize=1):
+		self.spamQueue = []
+		self.size = maxSize*10
+		self.repeat = self.size / 4
+		self.wait = self.repeat
+
+	def add(self,msg):
+		current = len(self.spamQueue)
+		if current < self.size or current == 0:
+			self.spamQueue.append(msg)
+		else:
+			self.spamQueue.pop(0)
+			self.spamQueue.append(msg)
+
+	def check(self):
+		# Wait a bit if we have already contributed
+		if self.timeout == -1:
+			if self.wait > 0:
+				if self.spamQueue.count(self.spamQueue[0]) > self.repeat:
+					self.timeout = time.time()
+					self.wait = self.repeat
+					return self.spamQueue[0]
+			elif (time.time() - self.timeout) > self.timeoutLimit:
+				self.timeout = -1
+				return None
+			else:
+				self.wait -= 1
+			# else wait for spam to happen
+		else:
+			print 'Gonna wait a bit to not get auto-banned.'
+
 def main():
+	k = Kappa()
 	try:
-		Kappa()
+		k.start()
 	except KeyboardInterrupt:
-		Kappa.quit()
+		k.quit()
 	sys.exit(0)
 
 if __name__ == "__main__":
