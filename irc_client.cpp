ae907a34be4295de60f5f68862e08bfcc41a469a
devincarr <devin.carr0@gmail.com>
2015-12-30 15:40:54 -0600

    Added license text.

--- irc_client.cpp
+++ irc_client.cpp
@@ -1,3 +1,14 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2015 Devin Carr
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ */
+
 #include "irc_client.h"
 
 IRCClient::IRCClient(Log* l): log(l), irc(IRCSocket(l)) { }
